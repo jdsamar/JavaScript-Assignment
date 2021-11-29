1410218d68ff7d38c86d0375276a89fa48c0a741
jdsamar <samarjeet360@gmail.com>
2021-11-29 15:46:34 +0530

    Run Callback Commit

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,4 +1,7 @@
 function RunCallback(a, b, cb) {
+    let x = a + b;
+
+    return cb(x);
     // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
 }
 
