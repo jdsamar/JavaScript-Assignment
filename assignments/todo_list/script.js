4c5f3d992eb7068828f88693cd792b7ed4e15d6a
jdsamar <samarjeet360@gmail.com>
2021-12-20 01:11:59 +0530

    Api Commit

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,20 +1,42 @@
-var input = document.querySelector('input');
 var button = document.querySelector('button');
-var todos = document.querySelector('div.todos');
+var todosDiv = document.querySelector('div.todos');
 
-var count = 0;
+let todos = [];
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => {
+//         const todos = json.slice(0,10);
+//         console.log(todos);
+//     })
+//     .catch(e => console.log(e));
+
+async function main() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+        const json = await response.json();
+        todos = json.slice(0,10);
+        console.log(todos);
+        todos.forEach((todo, index) =>{
+            const newTodo = document.createElement('p');
+            newTodo.setAttribute('key', index);
+            newTodo.innerHTML = todo.title
+            todosDiv.appendChild(newTodo);
+        })
+    } catch(e) {
+        console.log(e);
+    }
+}
 
 button.addEventListener('click', function(){
-    if (input.value){
-        var newTodo = document.createElement('p');
-        newTodo.innerHTML = input.value;
-        newTodo.setAttribute('key',count++);
-        todos.appendChild(newTodo);
-        newTodo.addEventListener('click',function(e){
-            var key = e.target.getAttribute('key');
-            document.querySelector(`p[key="${key}"]`).remove();
-        })
-        input.value = "";
-    }
-
+    todosDiv.innerHTML = "";
+    todos.filter(todo => todo.completed).forEach((todo, index) =>{
+        const newTodo = document.createElement('p');
+        newTodo.setAttribute('key', index);
+        newTodo.innerHTML = todo.title;
+        todosDiv.appendChild(newTodo);
+    })
 })
+
+main();
